64da2bcf5213dd80eed8ac31ab0354fa88e369db
MKFeniks <44569532+MKFeniks@users.noreply.github.com>
2019-04-04 20:35:31 +0200

    Add files via upload

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Program
+    {
+        public delegate void Notification (string name, string mesage);
+        public delegate void NotificationWithUser(User user, string mesage);
+
+        public static void Sms(string name, string sms)
+            {
+                Console.WriteLine($"Mr {name} you hawe receved sms:{sms}");
+            }
+            public static void Mms(string name, string mms)
+            {
+                Console.WriteLine($"Mr {name} you hawe receved mms:{mms}");
+            }
+
+            public static void Email (string name, string email)
+            {
+                Console.WriteLine($"Mr {name} you hawe receved email:{email}");
+             }
+        static void Main(string[] args)
+        {
+            Notification one = new Notification(Sms);
+            one += Mms;
+            one += Email;
+            one("Kristijan", "You are learning delegates");
+            Console.ReadKey();
+
+
+            User Pero = new User("Pero", "Perovski", "070223322", " peroperovski@delegate.sedc");
+            var delUser = new NotificationWithUser(SendEmail);
+
+            delUser += SendSms;
+            delUser(Pero, "Kazi zdravo be peer");
+
+            Console.Read();
+        }
+
+        public static void SendEmail(User user, string msg)
+        {
+            Console.WriteLine($"Sending {msg} to {user.Email}");
+        }
+
+        public static void SendSms(User user, string msg)
+        {
+            Console.WriteLine($"Sending {msg} to {user.PhoneNumber}");
+        }
+    }
+}
---
+++ Delegate/Delegate/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public User(string name,string lastName,string phoneNumber,string email)
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+                
+
+        }
+
+    }
+}
